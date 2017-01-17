71bdd5b2c6cb60a460716d53733a23667f785613
Mari Lliguicota <marilliguicota@gmail.com>
2017-01-16 21:44:57 -0800

    initial with cow

---
+++ HelloTriangle/HelloMetal/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  HelloMetal
+//
+//  Created by Warren Moore on 9/24/16.
+//  Copyright (c) 2016 Warren Moore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
