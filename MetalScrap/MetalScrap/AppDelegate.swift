6038c97fcfaf415a44b22228c89012760422a270
Mari Lliguicota <marilliguicota@gmail.com>
2017-01-31 22:42:36 -0800

    setting up to push to master

---
+++ MetalScrap/MetalScrap/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MetalScrap
+//
+//  Created by Mari Lliguicota on 1/29/17.
+//  Copyright (c) 2017 Mari Lliguicota. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
