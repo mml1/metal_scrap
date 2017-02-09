52a8d940a5bf2558304fb18dbe5479dab2098f67
Mari Lliguicota <marilliguicota@gmail.com>
2017-02-09 08:25:10 -0800

    previous work

--- MetalScrap/MetalScrap/GameViewController.swift
+++ MetalScrap/MetalScrap/GameViewController.swift
@@ -1,83 +1,204 @@
-//
-//  GameViewController.swift
-//  MetalScrap
-//
-//  Created by Mari Lliguicota on 1/29/17.
-//  Copyright (c) 2017 Mari Lliguicota. All rights reserved.
-//
 
 import Cocoa
 import MetalKit
 import simd
 
-let MaxBuffers = 3
-let ConstantBufferSize = 1024*1024
+//// triangle shape
+//let vertexPositionData:[Float] =
+//[
+//    -0.0,   0.25, 0.5, 1.0,
+//    -0.25, -0.25, 0.5, 1.0,
+//     0.25, -0.25, 0.5, 1.0
+//]
+//// triangle color
+//let vertexColorData:[Float] =
+//[
+//    0.0, 0.0, 1.0, 1.0,
+//    0.0, 0.0, 0.0, 1.0,
+//    1.0, 0.0, 0.0, 1.0
+//]
+// used to center the photo image one the triangle
+//let vertexTextCoords:[Float] =
+//    [
+//        0.5, 0,
+//        0, 1,
+//        1,1
+//]
 
-let vertexData:[Float] =
-[
-    -1.0, -1.0, 0.0, 1.0,
-    -1.0,  1.0, 0.0, 1.0,
-    1.0, -1.0, 0.0, 1.0,
-    
-    1.0, -1.0, 0.0, 1.0,
-    -1.0,  1.0, 0.0, 1.0,
-    1.0,  1.0, 0.0, 1.0,
-    
-    -0.0, 0.25, 0.0, 1.0,
-    -0.25, -0.25, 0.0, 1.0,
-    0.25, -0.25, 0.0, 1.0
+// square shape
+let vertexPositionData:[Float] =
+    [
+        0.0, 1.0, 0.5, 1.0,
+        1.0, 1.0, 0.5, 1.0,
+        0.0, 0.0, 0.5, 1.0,
+        1.0, 0.0, 0.5, 1.0,
+        0.0, 0.0, 0.5, 1.0,
+        1.0, 1.0, 0.5, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0,
+        0.0, 1.0, 0.5, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0,
+        1.0, 1.0, 0.5, 1.0,
+        0.0, 1.0, 0.5, 1.0,
+        0.0, 1.0, 0.5, 1.0,
+        0.0, 0.0, 0.5, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.5, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.5, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.5, 1.0,
+        1.0, 0.0, 0.5, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 0.5, 1.0,
+        1.0, 0.0, 0.5, 1.0,
+        1.0, 1.0, 0.0, 1.0,
+        1.0, 0.0, 0.5, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0
 ]
-
+// square color
 let vertexColorData:[Float] =
-[
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 1.0, 0.0, 1.0,
-    1.0, 0.0, 0.0, 1.0
+    [
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0
 ]
+
+// used to center the photo image one the triangle
+let vertexTextCoords:[Float] =
+    [
+        0, 0,
+        1, 0,
+        0, 1,
+        1, 1,
+        0, 1,
+        1, 0,
+        0, 0,
+        1, 0,
+        0, 1,
+        1, 1,
+        0, 1,
+        1, 0,
+        0, 0,
+        1, 0,
+        0, 1,
+        1, 1,
+        0, 1,
+        1, 0
+        
+]
+
+func rotationZ(rad: Float) -> float4x4 {
+    return float4x4([
+        float4( cos(rad), sin(rad), 0, 0),
+        float4(-sin(rad), cos(rad), 0, 0),
+        float4(        0,        0, 1, 0),
+        float4(        0,        0, 0, 1)])
+}
+
+// rotation y
+
+func rotationY(rad: Float) -> float4x4 {
+    return float4x4([
+        float4( cos(rad), 0, -sin(rad), 0),
+        float4( 0, 1, 0, 0),
+        float4(        sin(rad),0, cos(rad), 0),
+        float4(        0,        0, 0, 1)])
+}
+
+// translation function
+func translation(tx: Float, ty: Float, tz: Float) -> float4x4{
+    return float4x4([
+        float4(1,0,0,0),
+        float4(0,1,0,0),
+        float4(0,0,1,0),
+        float4(tx,ty,tz,1)
+        ])
+}
+// projection matrix
+func projectionMatrix(rad: Float, ar: Float, nearZ:Float, farZ: Float) -> float4x4 {
+    let tanHalfFOV = tan(rad/2)
+    let zRange = nearZ - farZ
+    
+    //col major so program expects the columns
+    return float4x4([
+        float4((1/tanHalfFOV),0,0,0),
+        float4(0,(1/tanHalfFOV*ar),0,0),
+        float4(0,0,((-nearZ-farZ)/zRange), 1),
+        float4(0,0,((farZ*nearZ)/zRange),0)
+        ])
+}
 
 class GameViewController: NSViewController, MTKViewDelegate {
     
+    //member variables
     var device: MTLDevice! = nil
-    
+    var startTime: TimeInterval = 0
+    var texture: MTLTexture! = nil // first make a texture member variable
+    var textureCoordinateBuffer: MTLBuffer! = nil // fourth  make texture buffer
     var commandQueue: MTLCommandQueue! = nil
     var pipelineState: MTLRenderPipelineState! = nil
-    var vertexBuffer: MTLBuffer! = nil
+    var vertexPositionBuffer: MTLBuffer! = nil
     var vertexColorBuffer: MTLBuffer! = nil
     
-    let inflightSemaphore = DispatchSemaphore(value: MaxBuffers)
-    var bufferIndex = 0
+    var mtkMesh: MTKMesh! = nil
+    var depthStencilState: MTLDepthStencilState! = nil
     
-    // offsets used in animation
-    var xOffset:[Float] = [ -1.0, 1.0, -1.0 ]
-    var yOffset:[Float] = [ 1.0, 0.0, -1.0 ]
-    var xDelta:[Float] = [ 0.002, -0.001, 0.003 ]
-    var yDelta:[Float] = [ 0.001,  0.002, -0.001 ]
-
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         device = MTLCreateSystemDefaultDevice()
-        guard device != nil else { // Fallback to a blank NSView, an application could also fallback to OpenGL here.
-            print("Metal is not supported on this device")
-            self.view = NSView(frame: self.view.frame)
-            return
-        }
-
+        
         // setup view properties
         let view = self.view as! MTKView
         view.delegate = self
         view.device = device
         view.sampleCount = 4
-        
+        view.depthStencilPixelFormat = .depth32Float_stencil8
         loadAssets()
     }
     
@@ -87,6 +208,11 @@ class GameViewController: NSViewController, MTKViewDelegate {
         let view = self.view as! MTKView
         commandQueue = device.makeCommandQueue()
         commandQueue.label = "main command queue"
+        let textureLoader = MTKTextureLoader(device:device) // second, load loader
+        let URL = Bundle.main.url(forResource: "spot_texture", withExtension: "png")
+        let options = [MTKTextureLoaderOptionOrigin : MTKTextureLoaderOriginBottomLeft as NSObject,
+                       MTKTextureLoaderOptionAllocateMipmaps : true as NSObject]
+        texture = try! textureLoader.newTexture(withContentsOf: URL!, options: options)
         
         let defaultLibrary = device.newDefaultLibrary()!
         let fragmentProgram = defaultLibrary.makeFunction(name: "passThroughFragment")!
@@ -96,6 +222,8 @@ class GameViewController: NSViewController, MTKViewDelegate {
         pipelineStateDescriptor.vertexFunction = vertexProgram
         pipelineStateDescriptor.fragmentFunction = fragmentProgram
         pipelineStateDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
+        pipelineStateDescriptor.depthAttachmentPixelFormat = view.depthStencilPixelFormat // expect to write to a depth map
+        pipelineStateDescriptor.stencilAttachmentPixelFormat = view.depthStencilPixelFormat // not using it yet but needed for build, helps restrict geometry in other geometry
         pipelineStateDescriptor.sampleCount = view.sampleCount
         
         do {
@@ -104,87 +232,112 @@ class GameViewController: NSViewController, MTKViewDelegate {
             print("Failed to create pipeline state, error \(error)")
         }
         
-        // generate a large enough buffer to allow streaming vertices for 3 semaphore controlled frames
-        vertexBuffer = device.makeBuffer(length: ConstantBufferSize, options: [])
-        vertexBuffer.label = "vertices"
+        let vertexPositionSize = vertexPositionData.count * MemoryLayout<Float>.size
+        vertexPositionBuffer = device.makeBuffer(bytes: vertexPositionData, length: vertexPositionSize, options: [])
+        vertexPositionBuffer.label = "vertices"
         
-        let vertexColorSize = vertexData.count * MemoryLayout<Float>.size
+        let vertexColorSize = vertexColorData.count * MemoryLayout<Float>.size
         vertexColorBuffer = device.makeBuffer(bytes: vertexColorData, length: vertexColorSize, options: [])
         vertexColorBuffer.label = "colors"
-    }
-    
-    func update() {
         
-        // vData is pointer to the MTLBuffer's Float data contents
-        let pData = vertexBuffer.contents()
-        let vData = (pData + 256 * bufferIndex).bindMemory(to:Float.self, capacity: 256 / MemoryLayout<Float>.stride)
         
-        // reset the vertices to default before adding animated offsets
-        vData.initialize(from: vertexData)
+        // load asset fifth
+        let vertexTextCoordSize = vertexTextCoords.count * MemoryLayout<Float>.size
+        textureCoordinateBuffer = device.makeBuffer(bytes: vertexTextCoords, length: vertexTextCoordSize, options: [])
+        textureCoordinateBuffer.label = "texture coodrinates"
         
-        // Animate triangle offsets
-        let lastTriVertex = 24
-        let vertexSize = 4
-        for j in 0..<3 {
-            // update the animation offsets
-            xOffset[j] += xDelta[j]
-            
-            if(xOffset[j] >= 1.0 || xOffset[j] <= -1.0) {
-                xDelta[j] = -xDelta[j]
-                xOffset[j] += xDelta[j]
-            }
-            
-            yOffset[j] += yDelta[j]
-            
-            if(yOffset[j] >= 1.0 || yOffset[j] <= -1.0) {
-                yDelta[j] = -yDelta[j]
-                yOffset[j] += yDelta[j]
-            }
-            
-            // Update last triangle position with updated animated offsets
-            let pos = lastTriVertex + j*vertexSize
-            vData[pos] = xOffset[j]
-            vData[pos+1] = yOffset[j]
-        }
+        startTime = CACurrentMediaTime()
+        
+        let allocator = MTKMeshBufferAllocator(device: device)
+        
+        // creating mesh 121116, switching over to interleaved buffer that stores all the properities
+        //        let mdlMesh = MDLMesh.init(boxWithExtent: vector_float3(1,1,1), segments: vector_uint3(2,2,2), inwardNormals: false, geometryType: .triangles, allocator: allocator)
+        
+        let vertexDescriptor = MDLVertexDescriptor()
+        vertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition, format: .float3, offset: 0, bufferIndex: 0)
+        vertexDescriptor.attributes[1] = MDLVertexAttribute(name: MDLVertexAttributeNormal, format: .float3, offset: 12, bufferIndex: 0)
+        vertexDescriptor.attributes[2] = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate, format: .float2, offset: 24, bufferIndex: 0)
+        vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: 32)
+        
+        let assetURL = Bundle.main.url(forResource: "spot", withExtension: "obj")!
+        let mdlAsset = MDLAsset(url: assetURL, vertexDescriptor: vertexDescriptor, bufferAllocator: allocator)
+        let mdlMesh = mdlAsset[0] as! MDLMesh
+        
+        mtkMesh = try! MTKMesh(mesh: mdlMesh, device: device)
+        
+        // depth buffering
+        let depthDescriptor = MTLDepthStencilDescriptor()
+        depthDescriptor.isDepthWriteEnabled = true // will not want for particle drawing, since drawing everything opaque
+        depthDescriptor.depthCompareFunction = .less // if it is closer to the camera overwrite
+        depthStencilState = device.makeDepthStencilState(descriptor: depthDescriptor)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let blitEncoder = commandBuffer.makeBlitCommandEncoder()
+        blitEncoder.generateMipmaps(for: texture)
+        blitEncoder.endEncoding()
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
     }
     
     func draw(in view: MTKView) {
         
-        // use semaphore to encode 3 frames ahead
-        let _ = inflightSemaphore.wait(timeout: DispatchTime.distantFuture)
-        
-        self.update()
-        
         let commandBuffer = commandQueue.makeCommandBuffer()
         commandBuffer.label = "Frame command buffer"
         
-        // use completion handler to signal the semaphore when this frame is completed allowing the encoding of the next frame to proceed
-        // use capture list to avoid any retain cycles if the command buffer gets retained anywhere besides this stack frame
-        commandBuffer.addCompletedHandler{ [weak self] commandBuffer in
-            if let strongSelf = self {
-                strongSelf.inflightSemaphore.signal()
-            }
-            return
-        }
-        
         if let renderPassDescriptor = view.currentRenderPassDescriptor, let currentDrawable = view.currentDrawable {
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.5, 1.0, 1.0)
             let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
             renderEncoder.label = "render encoder"
             
+            
+            let time = Float(CACurrentMediaTime()-startTime)
+            
+            let degreesToRadians = Float(M_PI / 180.0)
+            
+            //            let rotationMatrix = rotationZ(rad: 0 * time * degreesToRadians)
+            let rotationMatrix = rotationY(rad: 100 * time * degreesToRadians)
+            
+            let translate = translation(tx:0, ty:0,tz:-0.5)
+            let inverseTranslate = translation(tx:0, ty:0, tz:0.5 * time)
+            
+            //projection matrix
+            let projection = projectionMatrix(rad: Float.pi/2, ar: Float(self.view.bounds.size.width/self.view.bounds.size.height), nearZ:0.1, farZ:1000);
+            
+            //if I don't have the translate it will orbit around me lol
+            var modelProjectMatrix = projection * inverseTranslate * rotationMatrix * translate
+            
             renderEncoder.pushDebugGroup("draw morphing triangle")
             renderEncoder.setRenderPipelineState(pipelineState)
-            renderEncoder.setVertexBuffer(vertexBuffer, offset: 256*bufferIndex, at: 0)
-            renderEncoder.setVertexBuffer(vertexColorBuffer, offset:0 , at: 1)
-            renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 9, instanceCount: 1)
+            
+            //Location of vertex data for render call
+            //            renderEncoder.setVertexBuffer(vertexPositionBuffer, offset: 0, at: 0)
+            //            renderEncoder.setVertexBuffer(vertexColorBuffer, offset:0 , at: 1)
+            //            renderEncoder.setVertexBuffer(textureCoordinateBuffer, offset: 0, at: 2)
+            
+            renderEncoder.setVertexBuffer(mtkMesh.vertexBuffers.first!.buffer, offset: mtkMesh.vertexBuffers.first!.offset, at: 0)
+            //passing to shader, passing by address is the &
+            renderEncoder.setVertexBytes(&modelProjectMatrix, length: MemoryLayout<float4x4>.size, at: 3)
+            
+            
+            // step 3 adding to texture bucket (argument table)
+            renderEncoder.setFragmentTexture(texture, at: 0)
+            
+            //
+            renderEncoder.setDepthStencilState(depthStencilState)
+            
+            // used to draw the triangle or traingle strip
+            //renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4, instanceCount: 1)
+            //            renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 36, instanceCount: 1)
+            
+            let submesh =  mtkMesh.submeshes.first!
+            renderEncoder.drawIndexedPrimitives(type: .triangle, indexCount: submesh.indexCount, indexType: submesh.indexType, indexBuffer: submesh.indexBuffer.buffer, indexBufferOffset:submesh.indexBuffer.offset)
+            
             
             renderEncoder.popDebugGroup()
             renderEncoder.endEncoding()
             
             commandBuffer.present(currentDrawable)
         }
-        
-        // bufferIndex matches the current semaphore controled frame index to ensure writing occurs at the correct region in the vertex buffer
-        bufferIndex = (bufferIndex + 1) % MaxBuffers
         
         commandBuffer.commit()
     }
--- MetalScrap/MetalScrap/Shaders.metal
+++ MetalScrap/MetalScrap/Shaders.metal
@@ -1,34 +1,64 @@
+
 //
 //  Shaders.metal
 //  MetalScrap
 //
-//  Created by Mari Lliguicota on 1/29/17.
-//  Copyright (c) 2017 Mari Lliguicota. All rights reserved.
+//  Created by Warren Moore and Mari Lliguicota on 9/24/16.
+//  Copyright (c) 2016 Warren Moore and Mari Lliguicota. All rights reserved.
 //
 
 #include <metal_stdlib>
 
 using namespace metal;
 
-struct VertexInOut
+struct FragmentParameters
 {
     float4  position [[position]];
-    float4  color;
+    float4  normal;
+    float2  texCoords;
 };
 
-vertex VertexInOut passThroughVertex(uint vid [[ vertex_id ]],
-                                     constant packed_float4* position  [[ buffer(0) ]],
-                                     constant packed_float4* color    [[ buffer(1) ]])
+struct InputVertex
 {
-    VertexInOut outVertex;
-    
-    outVertex.position = position[vid];
-    outVertex.color    = color[vid];
-    
-    return outVertex;
+    packed_float3 position;
+    packed_float3 normal;
+    packed_float2 texCoords;
 };
 
-fragment half4 passThroughFragment(VertexInOut inFrag [[stage_in]])
+//vertex FragmentParameters passThroughVertex(uint vid [[ vertex_id ]],
+//                                            constant float4* position  [[ buffer(0) ]],
+//                                            constant float4* color    [[ buffer(1) ]],
+//                                            constant float4x4 &modelMatrix [[buffer(3)]],
+//                                            constant packed_float2* texCoords [[buffer(2)]])
+vertex FragmentParameters passThroughVertex(uint vid [[ vertex_id ]],
+                                            constant InputVertex* vertices  [[ buffer(0) ]],
+                                            constant float4x4 &modelMatrix [[buffer(3)]])
 {
-    return half4(inFrag.color);
+    InputVertex vert = vertices[vid]; // fetching all of the vertex properties with this call
+    
+    FragmentParameters out;
+    //passing into fragment buffer
+    out.position = modelMatrix * float4(float3(vert.position),1); // adds the w
+    out.normal    = float4(float3(vert.normal),0);
+    out.texCoords = vert.texCoords;
+    
+    return out;
+};
+
+fragment half4 passThroughFragment(FragmentParameters inFrag [[stage_in]],
+                                   texture2d<float, access::sample> tex2d [[texture(0)]])
+{
+    float3 lightDir = normalize(float3(1,-1,-1)); // from right and above light
+    float ambient = 0.2;
+    
+    float3 normal = normalize(inFrag.normal.xyz);
+    float diffuse = saturate(dot(-lightDir, normal)); // diffused intensity
+    
+    
+    constexpr sampler sampler2d(coord::normalized, filter::linear, mip_filter::linear, address::repeat);
+    return half4( (ambient + diffuse)* tex2d.sample(sampler2d, inFrag.texCoords));
+    
+    //    return half4(tex2d.sample(sampler2d, inFrag.texCoords));
+    //sample fxn if I look up at that coordinate what value do i get, return as float4 cast to half4 due to obligations
+    //    return half4(inFrag.color);
 };
